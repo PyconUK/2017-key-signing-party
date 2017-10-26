1c1b2df43d527bf9582478f56f9dcbec7a362442
Thomas Grainger <tom@procensus.com>
2017-10-26 15:14:42 +0100

    add tool to render a table

---
+++ table.mjs
@@ -0,0 +1,55 @@
+import util from 'util';
+import _ from 'lodash/fp';
+import fs from 'mz/fs';
+import glob from 'glob';
+import yaml from 'js-yaml';
+import markdownTable from 'markdown-table';
+
+const globP = util.promisify(glob);
+
+const chunkStr = n => _.flow(_.chunk(n), _.map(_.join('')));
+
+const formatFpHalf = _.flow(
+  chunkStr(4),
+  _.join(' '),
+)
+
+const formatFingerprint = _.flow(
+  _.replace(/(^0x|\s)/g, ''),
+  _.toLower,
+  chunkStr(20),
+  _.map(formatFpHalf),
+  _.join('  '),
+);
+
+
+const toTable = _.flow(
+  _.sortBy('name'),
+  _.map(v => ([v.name, v.fingerprint, '', ''])),
+)
+
+
+async function row(filename) {
+  const content = await fs.readFile(filename);
+  const { name, fingerprint: rawFingerprint } = yaml.safeLoad(content);
+  return { name, fingerprint: formatFingerprint(rawFingerprint)};
+}
+
+
+
+async function go() {
+  try {
+    const files = await globP('keys/*');
+    const rows = await Promise.all(_.map(row, files));
+    const tableRows = toTable(rows);
+    console.log(markdownTable([
+      ['name', 'fingerprint', 'fp verified', 'id verified'],
+      ...tableRows,
+    ]));
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+go();
